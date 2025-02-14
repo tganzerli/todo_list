e24a06ad484cf44d449308489bd688efee9a6061
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-02-14 17:33:59 -0300

    Update contracts.dart

--- lib/core/contracts/contracts.dart
+++ lib/core/contracts/contracts.dart
@@ -1,5 +1,6 @@
 export 'command.dart';
-export 'value_object.dart';
 export 'data_transfer_object.dart';
 export 'entity.dart';
+export 'parameters.dart';
+export 'value_object.dart';
 export 'view_model.dart';
